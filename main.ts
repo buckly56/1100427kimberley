2152ffd4a836104351b47595cd5c354a5ea0dcf0
buckly56 <45410163+buckly56@users.noreply.github.com>
2021-04-27 19:29:20 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
-basic.showArrow(ArrowNames.North)
-basic.pause(2)
+let a = 0
+input.onButtonPressed(Button.A, function () {
+    a = 5
+    basic.showNumber(a)
+    basic.pause(100)
+    for (let index = 0; index < 4; index++) {
+        a += 1
+        basic.showNumber(a)
+    }
+})
 basic.forever(function () {
 	
 })
